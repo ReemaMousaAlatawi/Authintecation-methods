e7cc147e5cf5d3cebc7fb280bbb6fbdaca5a3c85
Reema Mousa <Reea@hotmail.com>
2022-04-16 08:45:46 +0300

    Initial Commit

---
+++ LoginMethodsAndSignup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginMethodsAndSignup
+//
+//  Created by Reema Mousa on 15/09/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
